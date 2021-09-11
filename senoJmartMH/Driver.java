d4ef849cad1d91853b2c44338a8ce3016620dc3a
unknown <seno.aji91@ui.ac.id>
2021-09-11 11:18:12 +0700

    pesan commit

--- senoJmartMH/Driver.java
+++ senoJmartMH/Driver.java
@@ -2,20 +2,20 @@ package senoJmartMH;
 
 
 /**
- * Write a description of class Jmart here.
+ * Write a description of class Driver here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Jmart
+public class Driver
 {
     // instance variables - replace the example below with your own
     private int x;
 
     /**
-     * Constructor for objects of class Jmart
+     * Constructor for objects of class Driver
      */
-    public Jmart()
+    public Driver()
     {
         // initialise instance variables
         x = 0;
@@ -32,11 +32,8 @@ public class Jmart
         // put your code here
         return x + y;
     }
-}
-
-public class Driver
-{
-    public void main (String args[]){
+    
+        public void main (String args[]){
         getPromo();
     }
     
@@ -52,9 +49,27 @@ public class Driver
         if (before < after){
             return 0.0f;
         }else{
-            return(int)getDiscountPercentage;
+            return((before - after)/before)*100;
         }
     }
     
+    public float getDiscountedPrice(int price, float discountPercentage) {
+        if (discountPercentage > 100.0f){
+            return 0;
+        } else{
+            return (int)(price - (price*discountPercentage/100));
+        }
+    }
     
+    public float getCommissionMultiplier() {
+        return 0.05f;
+    }
+    
+    public int getAdjustedPrice(int price, float commissionMultiplier) {
+        return (int)(price + (price*commissionMultiplier/100));
+    }
+    
+    public float getAdminFee(int price, float commissionMultiplier){
+        return (int)(price*commissionMultiplier);
+    }
 }
