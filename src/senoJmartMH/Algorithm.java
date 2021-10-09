ea9ba5acce54e43bfc716bbef3cdee413998627e
unknown <seno.aji91@ui.ac.id>
2021-10-09 11:09:27 +0700

    CS modul 5 - revisi1

--- src/senoJmartMH/Algorithm.java
+++ src/senoJmartMH/Algorithm.java
@@ -7,116 +7,195 @@ public class Algorithm {
 
     private Algorithm() {}
 
-    public static <T>int count(T[] array, T value)
+    public static <T> int count(T[] array, T value)
     {
         int counter = 0;
-        Predicate predicate =(arg)-> {
-            if(value == arg){
-                return true;
-            }else{
-                return false;
-            }
-        };
-        for(T arrayValue : array){
-            if (predicate.predicate(arrayValue)){
+        for(T arrayValue : array)
+        {
+            if (arrayValue.equals(value))
+            {
                 counter++;
             }
         }
         return counter;
     }
 
-    public static <T> int count (Iterable <T> iterable, T value)
+    public static <T> int count(Iterable<T> iterable, T value)
     {
-        return 0;
+        int counter = 0;
+        for(T t : iterable)
+        {
+            if(t.equals(value))
+            {
+                counter++;
+            }
+        }
+        return counter;
     }
 
-    public static <T> int count (Iterator <T> iterator, T value)
+    public static <T> int count(Iterator<T> iterator, T value)
     {
-        return 0;
+        int counter = 0;
+        while(iterator.hasNext())
+        {
+            if(iterator.next().equals(value))
+            {
+                counter++;
+            }
+        }
+        return counter;
     }
 
-    public static <T> int count (T [] array, Predicate <T> pred)
+    public static <T> int count(T[] array, Predicate<T> pred)
     {
-        return 0;
+        int counter = 0;
+        for(T arrayValue : array)
+        {
+            if (arrayValue.equals(pred))
+            {
+                counter++;
+            }
+        }
+        return counter;
     }
 
-    public static <T> int count (Iterable <T> iterable, Predicate <T> pred)
+    public static <T> int count(Iterable<T> iterable, Predicate<T> pred)
     {
-        return 0;
+        int counter = 0;
+        for(T t : iterable)
+        {
+            if(t.equals(pred))
+            {
+                counter++;
+            }
+        }
+        return counter;
     }
 
-    public static <T> int count (Iterator <T> iterator, Predicate <T> pred)
+    public static <T> int count(Iterator<T> iterator, Predicate<T> pred)
     {
-        return 0;
+        int counter = 0;
+        while(iterator.hasNext())
+        {
+            if(iterator.next().equals(pred))
+            {
+                counter++;
+            }
+        }
+        return counter;
     }
 
-    public static <T> boolean exists (T [] array, T value)
+    public static <T> boolean exist(T[] array, T value)
     {
+        for(T arrayValue : array)
+        {
+            if (arrayValue.equals(value))
+            {
+                return true;
+            }
+        }
         return false;
     }
 
-    public static <T> boolean exists (Iterable <T> iterable, T value)
+    public static <T> boolean exist(Iterable<T> iterable, T value)
     {
+        for(T t : iterable)
+        {
+            if(t.equals(value))
+            {
+                return true;
+            }
+        }
         return false;
     }
 
-    public static <T> boolean exists (Iterator <T> iterator, T value)
+    public static <T> boolean exist(Iterator<T> iterator, T value)
     {
+        while(iterator.hasNext())
+        {
+            if(iterator.next().equals(value))
+            {
+                return true;
+            }
+        }
         return false;
     }
 
-    public static <T> boolean exists (T [] array, Predicate <T> pred)
+    public static <T> boolean exist(T[] array, Predicate<T> pred)
     {
+        for(T arrayValue : array)
+        {
+            if (arrayValue.equals(pred))
+            {
+                return true;
+            }
+        }
         return false;
     }
 
-    public static <T> boolean exists (Iterable <T> iterable, Predicate <T> pred)
+    public static <T> boolean exist(Iterable<T> iterable, Predicate<T> pred)
     {
+        for(T t : iterable)
+        {
+            if(t.equals(pred))
+            {
+                return true;
+            }
+        }
         return false;
     }
 
-    public static <T> boolean exists (Iterator <T> iterator, Predicate <T> pred)
+    public static <T> boolean exist(Iterator<T> iterator, Predicate<T> pred)
     {
+        while(iterator.hasNext())
+        {
+            if(iterator.next().equals(pred))
+            {
+                return true;
+            }
+        }
         return false;
     }
 
-    public static <T> T find (T [] array, T value)
+    public static <T> T find(T[] array, T value)
     {
         return null;
     }
 
-    public static <T> T find (Iterable <T> iterable, T value)
+    public static <T> T find(Iterable<T> iterable, T value)
     {
         return null;
     }
 
-    public static <T> T find (Iterator <T> iterator, T value)
+    public static <T> T find(Iterator<T> iterator, T value)
     {
         return null;
     }
 
-    public static <T> T find (T [] array, Predicate <T> pred)
+    public static <T> T find(T[] array, Predicate<T> pred)
     {
         return null;
     }
 
-    public static <T> T find (Iterable <T> iterable, Predicate <T> pred)
+    public static <T> T find(Iterable<T> iterable, Predicate<T> pred)
     {
         return null;
     }
 
-    public static <T> T find (Iterator <T> iterator, Predicate <T> pred)
+    public static <T> T find(Iterator<T> iterator, Predicate<T> pred)
     {
         return null;
     }
 
-    public static <T> T max (T first, T second)
+    public static<T extends Comparable<? super T>> T max(T first, T second)
     {
-        return null;
+        if(first.compareTo(second) > 0) return first;
+        return second;
     }
 
-    public static <T> T min (T first, T second)
+    public static<T extends Comparable<? super T>> T min(T first, T second)
     {
-        return null;
+        if(first.compareTo(second) < 0) return first;
+        return second;
     }
 }
