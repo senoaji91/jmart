24092f1e52c1a5a90ca287aaa75f6fd6f4e9b7f6
unknown <seno.aji91@ui.ac.id>
2021-11-05 20:43:18 +0700

    TP Modul 6

--- src/senoJmartMH/Jmart.java
+++ src/senoJmartMH/Jmart.java
@@ -1,25 +1,46 @@
 package senoJmartMH;
 
+import java.io.BufferedReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-/**
- * Write a description of class Jmart here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
+
 public class Jmart
-{    
+{
+    class Country
+    {
+        public String name;
+        public int population;
+        public List<String> listOfStates;
+    }
     public static void main (String[] args)
     {
-        System.out.println(Shipment.Duration.KARGO.getEstimatedArrival(new Date()));
-        Store store = new Store(1,"Test Name", "Test Address", "08123456789");
-        store.validate();
-        Complaint complaint = new Complaint(001, "Pengiriman tidak cepat, kurir tersesat");
-        System.out.print(complaint);
-        Account account = new Account(5,"Nama Akun","namaakun@gmail.com","passnamaakun");
-        account.validate();
-        System.out.print(account);
+        String filepath = "src/senoJmartMH/city.json";
+        Gson gson = new Gson();
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            Country input = gson.fromJson(br, Country.class);
+            System.out.println("name: " + input.name);
+            System.out.println("population: " + input.population);
+            System.out.println("states:");
+            input.listOfStates.forEach(state -> System.out.println(state));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+//        System.out.println(Shipment.Duration.KARGO.getEstimatedArrival(new Date()));
+//        Store store = new Store(1,"Test Name", "Test Address", "08123456789");
+//        store.validate();
+//        Complaint complaint = new Complaint(001, "Pengiriman tidak cepat, kurir tersesat");
+//        System.out.print(complaint);
+//        Account account = new Account(5,"Nama Akun","namaakun@gmail.com","passnamaakun");
+//        account.validate();
+//        System.out.print(account);
     }
    
     public static Product create()
